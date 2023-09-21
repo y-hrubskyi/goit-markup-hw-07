08ebbc0bb78d42a8863e1d98ab9fe47a785b3c5c
y-hrubskyi <y.hrubskyi@gmail.com>
2023-09-21 10:48:44 +0300

    Update modal.js
    Update closing modal window

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,48 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  backdrop: document.querySelector('.backdrop'),
+  modal: document.querySelector('[data-modal]'),
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener('click', onOpenModalBtnClick);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+function onOpenModalBtnClick(event) {
+  toggleModal();
+  addListenters();
+}
+
+function onCloseModalBtnClick() {
+  toggleModal();
+  removeListenters();
+}
+
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    toggleModal();
+    removeListenters();
   }
-})();
+}
+
+function onKeyUp(event) {
+  if (event.code === 'Escape') {
+    toggleModal();
+    removeListenters();
+  }
+}
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
+
+function addListenters() {
+  refs.closeModalBtn.addEventListener('click', onCloseModalBtnClick);
+  refs.backdrop.addEventListener('click', onBackdropClick);
+  document.addEventListener('keyup', onKeyUp);
+}
+
+function removeListenters() {
+  refs.closeModalBtn.removeEventListener('click', onCloseModalBtnClick);
+  refs.backdrop.removeEventListener('click', onBackdropClick);
+  document.removeEventListener('keyup', onKeyUp);
+}
